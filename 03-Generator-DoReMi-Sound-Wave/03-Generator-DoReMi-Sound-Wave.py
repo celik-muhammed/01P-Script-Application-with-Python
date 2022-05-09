527458b5cd11b5c877b22d5d58900a410cc7c0c1
allen <developerghost01@gmail.com>
2022-05-09 12:08:40 +0300

    two file added

--- 03-Generator-DoReMi-Sound-Wave/03-Generator-DoReMi-Sound-Wave.py
+++ 03-Generator-DoReMi-Sound-Wave/03-Generator-DoReMi-Sound-Wave.py
@@ -1,6 +1,12 @@
+# HapyBirthday-DoReMi
+# step-1 Define Python Library
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import scitools.sound
 
+# step-2 Define Function for Generate Note
 # Generate Note Tones
 def noteGenerator(frequency=440, length=0.2, amplitude=1, sample_rate=44100):
     '''
@@ -16,15 +22,14 @@ def noteGenerator(frequency=440, length=0.2, amplitude=1, sample_rate=44100):
     A = np.iinfo(np.int16).max  # Volume (Amplitude) - the volume measure (max is 32768) or the Peak Value
     '''
     timepoints = np.linspace(0, length, int(length*sample_rate))
-    data = amplitude*np.sin(2*np.pi*frequency*timepoints)  # A*sin(2*π*f*t) and .astype(np.float32)
+    data = amplitude*np.sin(2*np.pi*frequency*timepoints)  # A*sin(2*π*f*t)
     return data
 
-notes =   ('F3', 'F#3', 'G3',  'G#3',  'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4',  'G#4',  'A4')
-notesDo = ('Fa3','Fa#3','Sol3','Sol#3','La3','La#3','Si3','Do4','Do#4','Re4','Re#4','Mi4','Fa4','Fa#4','Sol4','Sol#4','La4')
-base_freq = 440*(2**(1/12))**-16  # edited for example
-notes2freq   = {notes[i].casefold(): base_freq*2**(i/12.0) for i in range(len(notes))}
-notesDo2freq = {notesDo[i].casefold(): base_freq*2**(i/12.0) for i in range(len(notes))}
+# step-3 Define Suitable Note Letter and Frequency for Process
+df = pd.read_csv('ScaleFreqs.csv', decimal =',')
+print(df)
 
+# step-4 Define Samle Composition Sheet
 composition_letters = """
 G3-0.4 G3-0.4 A3-0.8 G3-0.8 C4-0.7 B3-1.5 
 G3-0.4 G3-0.4 A3-0.8 G3-0.8 D4-0.7 C4-1.4 
@@ -32,31 +37,41 @@ G3-0.4 G3-0.4 G4-0.7 E4-0.7 C4-0.8 D4-0.8 E4-0.8
 F4-0.4 F4-0.4 E4-0.8 C4-0.8 D4-0.8 C4-1.6 
 """  # Happy Birthday to You - DoReMi-CDE
 
+# step-5 Generate all tone from defined Composition
 comp_letters_list = composition_letters.split()
 tones = []
 for letter in comp_letters_list:
     note, duration = letter.split('-')
-    if note.title() in notes:
-        s = noteGenerator(notes2freq[note.casefold()], float(duration))
+    if note.title() in df['notes'].values:
+        s = noteGenerator(df.loc[df['notes']==note.upper()]['frequency'].values[0], float(duration))
         tones.append(s)
-    elif note.title() in notesDo:
-        s = noteGenerator(notesDo2freq[note.casefold()], float(duration))
+    elif note.title() in df['notesDO'].values:
+        s = noteGenerator(df.loc[df['notesDO']==note.upper()]['frequency'].values[0], float(duration))
         tones.append(s)
     else:
         print('Someting is wrong! Check and Try Again.')
         pass
 
+# step-6 Make Note Data into a Single Array 
 tones_wave = (np.concatenate(tones)*(np.iinfo(np.int16).max))  # Amplitude run 1 time
 
+# step-7 Make Single Array into Double for Stereo Sound
 # A 2D array where the left and right tones are contained in their respective rows
 tones_wave_stereo = np.vstack((tones_wave, tones_wave))
 
+# step-8 Make double array available for registration
 # Reshape 2D array so that the left and right tones are contained in their respective columns
 tones_wave_stereo = tones_wave_stereo.transpose()
 
+# step-9 Save to Local Folder
 scitools.sound.write(data=tones_wave_stereo, filename="HapyBirthday-DoReMi-MuCe.wav", sample_rate=44100)
 
 # tones_wave_stereo features
 print(len(tones_wave_stereo))
 print(np.max(tones_wave_stereo))
 print(np.min(tones_wave_stereo))
+
+plt.plot(tones_wave_stereo[0:int(815845/440)])
+plt.xlabel('time')
+plt.ylabel('Amplitude')
+plt.show()
